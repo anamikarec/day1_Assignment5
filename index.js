58d210cbf9794287eafb30e8dec56bd3be9092fb
Anamika <anamikarecsonbhadra@gmail.com>
2022-01-27 00:06:16 +0530

    hackerrank boiler code of javascript is explained via comments

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-function runProgram(input){
-    // here we are splitting the input by newLine using "/n" 
-    var input=input.split("\n");
-    var n = Number(input[0]);
-    var arr=input[1].split(" ").map(Number);
- }
- /// process.stdin is an inbuilt application programming interface of the process module which listens the userinput
- //  process.stdin.resume() is a readable Stream that points to a standard input stream (stdin). Standard input streams are paused by default, so you must call process.
- process.stdin.resume();
- process.stdin.setEncoding("ascii");
- let read = "";
- process.stdin.on("data", function (input) {
-    read += input;
- });
- process.stdin.on("end", function () {
-    read = read.replace(/\n$/,"")
-   runProgram(read);
- });
+// HACKERRANK BOILER CODE IN JAVASCRIPT
+
+//declare variables to process the data
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+// Begin reading from stdin so the process does not exit. (basically reading from the command line)
+process.stdin.resume();
+
+//set the enconding for received data to ascii so it will be readable
+process.stdin.setEncoding('ascii');
+
+//if data is coming through, put it in the input_stdin string. keep receiving data until no more comes through
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+
+//after the transmission when the end signal is received break the string up and push each new line (\n == new line) as an element into the array.
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+
+//gives you one element per line to work with.
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
  
- process.on("SIGINT", function () {
-    read = read.replace(/\n$/,"")
-    runProgram(read);
-    process.exit(0);
- });
